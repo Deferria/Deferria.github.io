c048cfba0bfc30bb5ca4a3ff9aa32ecc6680b85e
Dedicatia <p314159265352022@outlook.com>
2026-05-12 18:08:17 +0800

    use HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './routes/root'
 import Home from './routes/home'
 import About from './routes/about'
 import Task from './routes/task'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />, 
-    children: [
-      { index: true, element: <div></div> }, 
-      { path: "about", element: <About /> }, 
-      { path: "home", element: <Home /> }, 
-      { path: "task", element: <Task /> },
-      { path: "*", element: <div>404 Not Found</div> },
-    ],
-  },
-])
-
+// use hash router to avoid 404 error when refreshing the page
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="task" element={<Task />} />
+      </Route>
+    </Routes>
+  </HashRouter>
 )
